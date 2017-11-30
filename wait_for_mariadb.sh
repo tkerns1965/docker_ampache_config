4c7478a112ed8f8bf6eac7a3b261f0028d04f3b9
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-30 14:10:55 -0500

    Update wait_for_mariadb.sh

--- wait_for_mariadb.sh
+++ wait_for_mariadb.sh
@@ -9,10 +9,10 @@ cmd="$@"
 
 # until psql -h "$host" -U "postgres" -c '\q'; do
 until  [[ $(docker inspect -f '{{json .State.Health.Status}}' ampache_mariadb1) == '"healthy"' ]]; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
+    >&2 echo "mariadb is unavailable - sleeping"
+    sleep 5
 done
 
->&2 echo "Postgres is up - executing command"
+>&2 echo "mariadb is up - executing command"
 # exec $cmd
 echo $cmd
