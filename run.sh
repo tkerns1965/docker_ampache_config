51740693293a5df3c1078d478a5096110a28b941
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-30 11:56:07 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker-compose build
 docker-compose up -d
-docker exec -it ampache_config1 bash /root/ampache_config.sh
-docker-compose down
+exec wait_for_mariadb.sh
+# docker exec -it ampache_config1 bash /root/ampache_config.sh
+# docker-compose down
