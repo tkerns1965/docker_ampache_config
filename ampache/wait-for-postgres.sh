7425d688d303e277be8483ce949227e844e8f8f5
tkerns1965 <tony.kerns.65@gmail.com>
2017-11-30 10:16:50 -0500

    Update wait-for-postgres.sh

--- ampache/wait-for-postgres.sh
+++ ampache/wait-for-postgres.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # wait-for-postgres.sh
 
-set -e
+# set -e
 
 host="$1"
 shift
 cmd="$@"
 
-until psql -h "$host" -U "postgres" -c '\q'; do
+# until psql -h "$host" -U "postgres" -c '\q'; do
 until  [[ $(docker inspect -f '{{json .State.Health.Status}}' ampache_mariadb1) == '"healthy"' ]]; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
-exec $cmd
+# exec $cmd
+echo $cmd
